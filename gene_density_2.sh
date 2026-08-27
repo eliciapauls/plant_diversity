c5b0d38d01c319ba1471bbb636eda4c35305c8d2
eliciapauls <139250850+eliciapauls@users.noreply.github.com>
2026-08-27 13:37:49 -0600

    Update gene_density_2.sh

--- gene_density_2.sh
+++ gene_density_2.sh
@@ -1,17 +1,17 @@
 ##add scaffold lengths to gene density output file 1
 #!/bin/bash
 
-# Input files
+#input files
 file1="Zmaysgenespaces.txt"
 file2="../Zmays/fasta_scaffolds_with_lengths.txt"
 output_file="Zmayscombined.txt"
 
-# Use awk to process the files
+#use awk to process the files
 awk '
   # Read File2.txt and store the second column in an array indexed by the first column
   FNR==NR { map[$1]=$2; next }
 
-  # For each line in File1.txt, append the corresponding value from the array
+  #for each line in file1.txt, append the corresponding value from the array
   { print $0, map[$1] }
 ' "$file2" "$file1" > "$output_file"
 
